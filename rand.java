fd0514089e9edde41588e4f242ddd39760a96943
amleo1 <56758498+amleo1@users.noreply.github.com>
2020-10-04 12:32:08 +0530

    Update rand.java
    added extra value

--- rand.java
+++ rand.java
@@ -12,17 +12,23 @@ public class generateRandom{
 		// Generate random integers in range 0 to 999 
 		int rand_int1 = rand.nextInt(1000); 
 		int rand_int2 = rand.nextInt(1000); 
+		int rand_int3 = rand.nextInt(500);
 
 		// Print random integers 
 		System.out.println("Random Integers: "+rand_int1); 
 		System.out.println("Random Integers: "+rand_int2); 
+		System.out.println("Random Integers: "+rand_int3); 
+		
+		
 
 		// Generate Random doubles 
 		double rand_dub1 = rand.nextDouble(); 
-		double rand_dub2 = rand.nextDouble(); 
+		double rand_dub2 = rand.nextDouble();
+		double rand_dub3 = rand.nextDouble();
 
 		// Print random doubles 
 		System.out.println("Random Doubles: "+rand_dub1); 
 		System.out.println("Random Doubles: "+rand_dub2); 
+		System.out.println("Random Doubles: "+rand_dub3);
 	} 
 } 
